12eb6075e9f6e9b3c7ff62906fe413a7778842d8
meogol <avs99.98@gmail.com>
2021-03-07 20:38:46 +0300

    Auto stash before rebase of "origin/main"

---
+++ DNFWPicPicker/src/main/java/form/UpdJPanel.java
@@ -0,0 +1,23 @@
+package form;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class UpdJPanel extends JPanel {
+    BufferedImage image;
+
+    @Override
+    public void paintComponent(Graphics g){
+        if(image == null) return;
+
+        g.drawImage(image, 0, 0, this);
+    }
+
+    public void updateBuffer(BufferedImage image){
+        if(image == null) return;
+
+        this.image=image;
+        repaint();
+    }
+}
