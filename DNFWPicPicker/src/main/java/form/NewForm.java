09d183db4ff8c834862924c39274aa0b4c0fbf15
EvgeniiKarelin <eugenefolkast@gmail.com>
2021-03-07 21:13:23 +0300

    bugfix

--- DNFWPicPicker/src/main/java/form/NewForm.java
+++ DNFWPicPicker/src/main/java/form/NewForm.java
@@ -8,10 +8,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
+import static javax.swing.JOptionPane.showMessageDialog;
+
 public class NewForm  extends JFrame {
     private UpdJPanel tilePanel = new UpdJPanel();
-    private JButton aboutButton = new JButton("");
-    private JButton showButton = new JButton("showButton");
+    private JButton aboutButton = new JButton("О программе");
+    private JButton showButton = new JButton("Вывести картинку");
     private MainLogic mainLogic = new MainLogic();
 
 
@@ -28,6 +30,15 @@ public class NewForm  extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 showRandomPic();
+            }
+        });
+        aboutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showMessageDialog(null,"Разработчики: Слепнёв Алексей Валерьевич и Карелин Евгений Альбертович.\n" +
+                        "СПБГУТ им.проф.М.А.Бонч-Бруевича, группа ИКПИ-91, факультет ИКСС.\n" +
+                        "Программа выводит случайную картинку из загруженных в папку программы.\n" +
+                        "Доступны для вывода только картинки формата *.jpg и *.png.");
             }
         });
     }
